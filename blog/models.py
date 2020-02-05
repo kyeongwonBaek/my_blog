60c033d111db7ca1b5705875b936951a9580c715
kyeongwonBaek <forte111@naver.com>
2020-02-05 13:46:45 +0900

    tag model tested

--- blog/models.py
+++ blog/models.py
@@ -12,6 +12,16 @@ class Category(models.Model):
         verbose_name_plural ='categories'
     def get_absolute_url(self):
         return '/blog/category/{}/'.format(self.slug)
+class Tag(models.Model):
+    name = models.CharField(max_length=40, unique=True)
+    slug = models.SlugField(unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    # def get_absolute_url(self):
+    #     return '/blog/category/{}/'.format(self.slug)
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
@@ -19,6 +29,7 @@ class Post(models.Model):
     created = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=True)
     category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.SET_NULL)
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return '{}::{}'.format(self.title, self.author)
