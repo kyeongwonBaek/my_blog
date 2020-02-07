f2e6dc463f581e73f282281cdde33912393df138
kyeongwonBaek <forte111@naver.com>
2020-02-07 13:13:28 +0900

    google login works

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,13 @@ INSTALLED_APPS = [
     'blog',
     'markdownx',
     'crispy_forms',
+    'django.contrib.sites',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.naver',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +136,16 @@ from datetime import datetime
 MARKDOWNX_MEDIA_PATH = datetime.now().strftime('markdownx/%Y/%m/%d')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+AUTHENTICATION_BACKENDS = (
+
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+
+)
+
+SITE_ID = 1
+ACCOUNT_EMAIL_REQUIRED =True
+
+
