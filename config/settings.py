2384e11e71a055d12c3bb6c114b93aec61dee998
kyeongwonBaek <forte111@naver.com>
2020-02-09 00:57:10 +0900

    only logged in user can comment

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5(az^3^fn5i0uleo%*i0v)f2e6mlf%)a65we6ux_%+#1yaqn^7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['139.180.203.221',  '127.0.0.1', 'kyeongkate.com', 'www.kyeongkate.com']
 
--- config/urls.py
+++ config/urls.py
@@ -15,12 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
 from django.conf import settings
 from django.views.static import serve
 from django.urls import re_path
-
-
 urlpatterns = [
     path('markdownx/', include('markdownx.urls')),
     path('admin/', admin.site.urls),
@@ -28,7 +25,4 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     path('', include('basecamp.urls')),
-
 ]
-
-
